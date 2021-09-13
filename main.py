883881888d4d15be6cf4e460e4e5ed3d697d0111
Andres Rojas <andres.rojas.90@gmail.com>
2021-09-13 17:09:56 -0300

    add load all files convert

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import sys
 from pydub import AudioSegment as au
+import glob
 
 
-sound = au.from_wav(sys.argv[1])
-sound.export(sys.argv[2], format="mp3")
+#sound = au.from_wav(sys.argv[1])
+#sound.export(sys.argv[2], format="mp3")
+
+#print(glob.glob("./wav/*.WAV"))
+
+for file in glob.glob("./wav/*.wav"):
+    sound = au.from_wav(file)
+    sound.export(file.replace('./wav', './mp3').replace('.wav', '.mp3'), format="mp3")
 
 
 
